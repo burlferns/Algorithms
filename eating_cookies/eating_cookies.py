50ef2d9c581e26c0bf3fe41c3d488ddf6c90f2a2
B F <burlferns@gmail.com>
2020-03-19 09:04:14 -0700

    faster version of eating_cookies.py

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,13 +8,42 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive
 # recursive solution
-def eating_cookies(n, cache=None):
+def eating_cookiesOFF(n, cache=None):
     if n < 0:
         return 0
     elif n == 0:
         return 1
 
     return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+
+
+############################################################
+# Second version
+############################################################
+# The cache parameter is here for if you want to implement
+# a solution that is more efficient than the naive
+# recursive solution
+def eating_cookies(n, cache=None):
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+
+
+    terms = [0, 0, 0, 0]
+    for i in [1, 2, 3]:
+        if cache != None and cache[n - i] != 0:
+            terms[i] = cache[n - i]
+        else:
+            terms[i] = eating_cookies(n - i, cache)
+            if cache != None:
+                cache[n - i] = terms[i]
+
+    sum = terms[1] + terms[2] + terms[3]
+
+    return sum
+
+
 
 
 
